1f196fc01714a1c548176899f07ab2c824f6059a
Erdi Maden <erdi.maden@coinbase.com>
2024-05-16 11:24:53 -0500

    updating JSDocs

--- src/coinbase/utils.ts
+++ src/coinbase/utils.ts
@@ -28,8 +28,8 @@ export const logApiResponse = (response: AxiosResponse, debugging = false): Axio
 /**
  * Axios Request interceptor function type.
  *
- * @param {InternalAxiosRequestConfig} value - The Axios request configuration.
- * @returns {InternalAxiosRequestConfig} The modified Axios request configuration.
+ * @param value - The Axios request configuration.
+ * @returns The modified Axios request configuration.
  */
 type RequestFunctionType = (
   value: InternalAxiosRequestConfig<any>,
@@ -38,17 +38,17 @@ type RequestFunctionType = (
 /**
  * Axios Response interceptor function type.
  *
- * @param {AxiosResponse} value - The Axios response object.
- * @returns {AxiosResponse} The modified Axios response object.
+ * @param value - The Axios response object.
+ * @returns The modified Axios response object.
  */
 type ResponseFunctionType = (value: AxiosResponse<any, any>) => AxiosResponse<any, any>;
 
 /**
  * Registers request and response interceptors to an Axios instance.
  *
- * @param {Axios} axiosInstance - The Axios instance to register the interceptors.
- * @param {RequestFunctionType} requestFn - The request interceptor function.
- * @param {ResponseFunctionType} responseFn - The response interceptor function.
+ * @param axiosInstance - The Axios instance to register the interceptors.
+ * @param requestFn - The request interceptor function.
+ * @param responseFn - The response interceptor function.
  */
 export const registerAxiosInterceptors = (
   axiosInstance: Axios,
@@ -64,8 +64,8 @@ export const registerAxiosInterceptors = (
 /**
  * Converts a Uint8Array to a hex string.
  *
- * @param {Uint8Array} key - The key to convert.
- * @returns {string} The converted hex string.
+ * @param key - The key to convert.
+ * @returns The converted hex string.
  */
 export const convertStringToHex = (key: Uint8Array): string => {
   return Buffer.from(key).toString("hex");
