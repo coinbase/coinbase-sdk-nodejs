018a4b53b60675c21e49360e5b6f0d82bc935974
Erdi Maden <erdi.maden@coinbase.com>
2024-06-18 18:54:16 -0500

    Updating test cases

--- src/tests/e2e.ts
+++ src/tests/e2e.ts
@@ -85,8 +85,12 @@ describe("Coinbase SDK E2E Test", () => {
     expect(unhydratedWallet.canSign()).toBe(true);
     expect(unhydratedWallet.getId()).toBe(walletId);
 
-    console.log("Transfering 1 Gwei from default address to second address...");
-    const transfer = await unhydratedWallet.createTransfer(1, Coinbase.assets.Gwei, wallet);
+    console.log("Transfering 0.000000001 ETH from default address to second address...");
+    const transfer = await unhydratedWallet.createTransfer(
+      0.000000001,
+      Coinbase.assets.Eth,
+      wallet,
+    );
     expect(await transfer.getStatus()).toBe(TransferStatus.COMPLETE);
     console.log(`Transferred 1 Gwei from ${unhydratedWallet} to ${wallet}`);
 
