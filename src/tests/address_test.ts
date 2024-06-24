c0fadc8be784c9e5e34de000aee36b7c69a58bf8
Marcin Lenczewski <marcin.lenczewski@coinbase.com>
2024-06-23 22:31:00 -0400

    Increased code coverage for wallet address, staking operation, and address classes

---
+++ src/tests/address_test.ts
@@ -0,0 +1,25 @@
+import { Address } from "../coinbase/address";
+import { VALID_ADDRESS_MODEL } from "./utils";
+
+describe("Address", () => {
+  describe(".getNetworkId", () => {
+    it("should get the network ID", () => {
+      const address = new Address(VALID_ADDRESS_MODEL.network_id, VALID_ADDRESS_MODEL.address_id);
+      expect(address.getNetworkId()).toEqual(VALID_ADDRESS_MODEL.network_id);
+    });
+  });
+  describe(".geId", () => {
+    it("should get the network ID", () => {
+      const address = new Address(VALID_ADDRESS_MODEL.network_id, VALID_ADDRESS_MODEL.address_id);
+      expect(address.getId()).toEqual(VALID_ADDRESS_MODEL.address_id);
+    });
+  });
+  describe(".toString()", () => {
+    it("should get the network ID", () => {
+      const address = new Address(VALID_ADDRESS_MODEL.network_id, VALID_ADDRESS_MODEL.address_id);
+      expect(address.toString()).toEqual(
+        `Address { addressId: '${VALID_ADDRESS_MODEL.address_id}', networkId: '${VALID_ADDRESS_MODEL.network_id}' }`,
+      );
+    });
+  });
+});
