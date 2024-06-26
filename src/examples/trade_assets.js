63a3c2ca95c772a037ac05f987e498f5078d0d28
Erdi Maden <erdi.maden@coinbase.com>
2024-06-26 17:35:42 -0500

    Updating examples

--- src/examples/trade_assets.js
+++ src/examples/trade_assets.js
@@ -8,7 +8,7 @@ async function tradeAssets() {
 
   // Fund the wallet's default address with ETH from an external source.
   // Trade 0.00001 ETH to USDC
-  const trade = await wallet.createTrade(0.00001, Coinbase.assets.Eth, Coinbase.assets.Usdc);
+  const trade = await wallet.trade(0.00001, Coinbase.assets.Eth, Coinbase.assets.Usdc);
   console.log(`Trade successfully completed: `, trade);
 }
 
