1af65a454fa0d53bc4e9682a9ebdb82b9da18aca
John Peterson <98187317+John-peterson-coinbase@users.noreply.github.com>
2024-07-31 16:34:24 -0400

    [PSDK-356] Mass Payout Quickstart Template (#124)
    * [PSDK-356] Mass Payout Quickstart Template
    
    * add trade section to README + fix casing on doc strings

--- quickstart-template/README.md
+++ quickstart-template/README.md
@@ -2,10 +2,32 @@
 
 This is a template repository for quickly getting started with the Platform SDK. It provides a simple example of how to use the SDK.
 
+## Create a Wallet, Fund, and Transfer
+
 To set up the template, run the following commands:
 ```bash
 npm install
-npm start
+npm run start
 ```
 
 This command will create a developer-custodial wallet, deposit testnet funds to it and perform a transfer to another wallet.
+
+## Trade Assets
+
+To set up the template, run the following commands:
+```bash
+npm install
+npm run start-trade-assets
+```
+
+This command will create a developer-custodial wallet on Base Mainnet and trade ETH for USDC.
+
+## Mass Payout
+
+To set up the template, run the following commands:
+```bash
+npm install
+npm run start-mass-payout
+```
+
+This command will demonstrate how to automatically send batched payments from a CSV file with a non-MPC API Wallet.
---
+++ quickstart-template/mass-payout.js
@@ -0,0 +1,104 @@
+import { Coinbase } from "@coinbase/coinbase-sdk";
+import { createArrayCsvWriter } from "csv-writer";
+import os from "os";
+import fs from "fs";
+import { parse } from "csv-parse";
+
+// Create receiving Wallets.
+async function createReceivingWallets(user) {
+  // Create 5 receiving Wallets and only store Wallet Addresses.
+  const addresses = [];
+
+  for (let i = 1; i <= 5; i++) {
+    let receivingWallet = await user.createWallet();
+    console.log(`Receiving Wallet${i} successfully created: `, receivingWallet.toString());
+
+    let receivingAddress = receivingWallet.getDefaultAddress();
+    console.log(`Default address for Wallet${i}: `, receivingAddress.getId());
+    addresses.push([receivingAddress.getId()]); // Storing Address as an array.
+  }
+
+  return addresses;
+}
+
+// Write to CSV file with receiving Wallet Addresses.
+async function writeReceivingAddressesToCsv(addresses) {
+  // Define CSV file.
+  const csvWriter = createArrayCsvWriter({
+    path: "wallet-array.csv",
+    header: false,
+  });
+
+  // Write Wallet Addresses to CSV file.
+  await csvWriter.writeRecords(addresses);
+  console.log("The CSV file was written successfully without headers.");
+}
+
+// Create and fund a sending Wallet.
+async function createAndFundSendingWallet(user) {
+  // Create sending Wallet.
+  let sendingWallet = await user.createWallet();
+  console.log(`sendingWallet successfully created: `, sendingWallet.toString());
+
+  // Get sending Wallet Address.
+  let sendingAddress = sendingWallet.getDefaultAddress();
+  console.log(`Default address for sendingWallet: `, sendingAddress.toString());
+
+  // Fund sending Wallet.
+  const faucetTransaction = await sendingWallet.faucet();
+  console.log(`Faucet transaction successfully completed: `, faucetTransaction.toString());
+
+  return sendingWallet;
+}
+
+// Read from CSV file and send mass payout.
+async function sendMassPayout(sendingWallet) {
+  // Define amount to send.
+  const transferAmount = 0.000002;
+  const assetId = Coinbase.assets.Eth;
+
+  try {
+    const parser = fs
+      .createReadStream("./wallet-array.csv")
+      .pipe(parse({ delimiter: ",", from_line: 1 }));
+
+    for await (const row of parser) {
+      const address = row[0];
+      if (address) {
+        try {
+          await sendingWallet.createTransfer({
+            // Send payment to each Address in CSV.
+            amount: transferAmount,
+            assetId: assetId,
+            destination: address,
+          });
+          console.log(`Transfer to ${address} successful`);
+        } catch (error) {
+          console.error(`Error transferring to ${address}: `, error);
+        }
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing CSV file: `, error);
+  }
+
+  console.log("Finished processing CSV file");
+}
+
+(async () => {
+  try {
+    // Manage CDP Api Key for Coinbase SDK.
+    // Configure location to CDP API Key.
+    let coinbase = Coinbase.configureFromJson({
+      filePath: `${os.homedir()}/Downloads/cdp_api_key.json`,
+    });
+
+    const user = await coinbase.getDefaultUser();
+    const addresses = await createReceivingWallets(user);
+    await writeReceivingAddressesToCsv(addresses);
+    const sendingWallet = await createAndFundSendingWallet(user);
+    await sendMassPayout(sendingWallet);
+  } catch (error) {
+    console.error(`Error in sending mass payout: `, error);
+  }
+})();
