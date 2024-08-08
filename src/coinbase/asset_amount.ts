4aadb9697a265fb207ac0c37583ba1d13a51e1de
marc.xu <marc.xu@coinbase.com>
2024-08-07 18:49:52 -0700

    lint

--- src/coinbase/asset_amount.ts
+++ src/coinbase/asset_amount.ts
@@ -7,7 +7,15 @@ export class AssetAmount {
   public readonly raw_numeric: string;
   public readonly exp: number;
 
-  public constructor(amount: string, raw_numeric: string, exp: number,  ticker: string) {
+  /**
+   * Public constructor for the AssetAmount class.
+   *
+   * @param amount - the amount
+   * @param raw_numeric - the raw numeric
+   * @param exp - the exp
+   * @param ticker - the ticker
+   */
+  public constructor(amount: string, raw_numeric: string, exp: number, ticker: string) {
     this.amount = amount;
     this.ticker = ticker;
     this.raw_numeric = raw_numeric;
@@ -21,30 +29,55 @@ export class AssetAmount {
    * @returns {AssetAmount} The Balance object.
    */
   public static fromModel(model: AssetAmountModel): AssetAmount {
-    return new AssetAmount(
-      model.amount,
-      model.raw_numeric,
-      model.exp,
-      model.ticker,
-    );
+    return new AssetAmount(model.amount, model.raw_numeric, model.exp, model.ticker);
   }
 
+  /**
+   * Returns the amount.
+   *
+   * @returns {string} The amount.
+   * @memberof AssetAmount
+   */
   public getAmount(): string {
     return this.amount;
   }
 
+  /**
+   * Returns the ticker.
+   *
+   * @returns {string} The ticker.
+   * @memberof AssetAmount
+   */
   public getTicker(): string {
     return this.ticker;
   }
 
+  /**
+   * Returns the raw numeric.
+   *
+   * @returns {string} The raw numeric.
+   * @memberof AssetAmount
+   */
   public getRawNumeric(): string {
     return this.raw_numeric;
   }
 
+  /**
+   * Returns the exp.
+   *
+   * @returns {number} The exp.
+   * @memberof AssetAmount
+   */
   public getExp(): number {
     return this.exp;
   }
 
+  /**
+   * Returns the string representation of a AssetAmount.
+   *
+   * @returns {string} The string representation of a AssetAmount.
+   * @memberof AssetAmount
+   */
   public toString(): string {
     return `{ amount: '${this.amount}', raw_numeric: '${this.raw_numeric}', exp: ${this.exp}, ticker: '${this.ticker}' }`;
   }
