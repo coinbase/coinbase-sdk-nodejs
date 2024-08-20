301f1632a4e6e0dfeaad03fb8296dd1981718151
Alex Stone <alex.stone@coinbase.com>
2024-08-20 08:58:42 -0700

    [chore] Cleanup unused constants

--- src/coinbase/constants.ts
+++ src/coinbase/constants.ts
@@ -1,5 +1,1 @@
-import { Decimal } from "decimal.js";
-
 export const GWEI_DECIMALS = 9;
-export const WEI_PER_ETHER = new Decimal("1000000000000000000");
-export const ATOMIC_UNITS_PER_USDC = new Decimal("1000000");
