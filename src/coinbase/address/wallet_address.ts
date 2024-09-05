99b516ba22da01c0d2221070a00d6bb0f61d5857
rohan-agarwal-coinbase <rohan.agarwal.1@coinbase.com>
2024-09-04 21:21:27 -0400

    Added ability to get private key for WalletAddress and updated yarn.lock file (#213)
    * Added ability to get private key for WalletAddress and updated yarn.lock file
    
    * Add semicolon
    
    * update
    
    * Refactored tests
    
    * lint
    
    * address feedback
    
    * Changes

--- src/coinbase/address/wallet_address.ts
+++ src/coinbase/address/wallet_address.ts
@@ -75,6 +75,19 @@ export class WalletAddress extends Address {
   }
 
   /**
+   * Exports the Address's private key to a hex string.
+   *
+   * @returns The Address's private key as a hex string. 
+   */
+  public export() {
+    if (this.key === undefined) {
+      throw new Error("Private key is not set");
+    }
+
+    return this.key.privateKey;
+  }
+
+  /**
    * Returns whether the Address has a private key backing it to sign transactions.
    *
    * @returns Whether the Address has a private key backing it to sign transactions.
