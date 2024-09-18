cd0bbe18f08b1ec63def4555e10120b338706141
Filipe Azevedo <filipeandre.martaazevedo@coinbase.com>
2024-09-18 10:06:22 +0100

    update generated client

--- src/examples/solana_list_rewards.ts
+++ src/examples/solana_list_rewards.ts
@@ -1,18 +1,19 @@
 import { StakingReward } from "../coinbase/staking_reward";
 import { Coinbase } from "../coinbase/coinbase";
 import { Address } from "../coinbase/address";
+import { NetworkIdentifier } from "../client";
 
-async function printStakingInfo() {
+async function listSolanaStakingRewards() {
   Coinbase.configureFromJson({ filePath: "~/Downloads/cdp_api_key.json" });
 
   const startTime = new Date(2024, 5).toISOString();
 
-  const rewards = await StakingReward.list("solana-mainnet", Coinbase.assets.Sol, ["beefKGBWeSpHzYBHZXwp5So7wdQGX6mu4ZHCsH3uTar"], startTime, new Date().toISOString());
+  const rewards = await StakingReward.list(NetworkIdentifier.SolanaMainnet, Coinbase.assets.Sol, ["beefKGBWeSpHzYBHZXwp5So7wdQGX6mu4ZHCsH3uTar"], startTime, new Date().toISOString());
   console.log(rewards);
 
-  const addr = new Address("solana-mainnet", "beefKGBWeSpHzYBHZXwp5So7wdQGX6mu4ZHCsH3uTar");
+  const addr = new Address(NetworkIdentifier.SolanaMainnet, "beefKGBWeSpHzYBHZXwp5So7wdQGX6mu4ZHCsH3uTar");
   const balances = await addr.historicalStakingBalances(Coinbase.assets.Sol, startTime, new Date().toISOString());
   console.log(balances);
 }
 
-printStakingInfo();
+listSolanaStakingRewards();
