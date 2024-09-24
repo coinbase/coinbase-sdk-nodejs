522e499a27107a32657b36e839ce83481621c881
Howard Xie <howard.xie@coinbase.com>
2024-09-24 14:24:48 -0700

    Update api.ts

--- src/client/api.ts
+++ src/client/api.ts
@@ -786,6 +786,25 @@ export interface CreateWalletRequestWallet {
 /**
  * 
  * @export
+ * @interface CreateWalletWebhookRequest
+ */
+export interface CreateWalletWebhookRequest {
+    /**
+     * The URL to which the notifications will be sent
+     * @type {string}
+     * @memberof CreateWalletWebhookRequest
+     */
+    'notification_uri': string;
+    /**
+     * The custom header to be used for x-webhook-signature header on callbacks, so developers can verify the requests are coming from Coinbase.
+     * @type {string}
+     * @memberof CreateWalletWebhookRequest
+     */
+    'signature_header'?: string;
+}
+/**
+ * 
+ * @export
  * @interface CreateWebhookRequest
  */
 export interface CreateWebhookRequest {
@@ -9289,6 +9308,44 @@ export class WalletsApi extends BaseAPI implements WalletsApiInterface {
 export const WebhooksApiAxiosParamCreator = function (configuration?: Configuration) {
     return {
         /**
+         * Create a new webhook scoped to a wallet
+         * @summary Create a new webhook scoped to a wallet
+         * @param {string} walletId The ID of the wallet to create the webhook for.
+         * @param {CreateWalletWebhookRequest} [createWalletWebhookRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createWalletWebhook: async (walletId: string, createWalletWebhookRequest?: CreateWalletWebhookRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'walletId' is not null or undefined
+            assertParamExists('createWalletWebhook', 'walletId', walletId)
+            const localVarPath = `/v1/wallets/{wallet_id}/webhooks`
+                .replace(`{${"wallet_id"}}`, encodeURIComponent(String(walletId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(createWalletWebhookRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
          * Create a new webhook
          * @summary Create a new webhook
          * @param {CreateWebhookRequest} [createWebhookRequest] 
@@ -9445,6 +9502,20 @@ export const WebhooksApiFp = function(configuration?: Configuration) {
     const localVarAxiosParamCreator = WebhooksApiAxiosParamCreator(configuration)
     return {
         /**
+         * Create a new webhook scoped to a wallet
+         * @summary Create a new webhook scoped to a wallet
+         * @param {string} walletId The ID of the wallet to create the webhook for.
+         * @param {CreateWalletWebhookRequest} [createWalletWebhookRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async createWalletWebhook(walletId: string, createWalletWebhookRequest?: CreateWalletWebhookRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Webhook>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.createWalletWebhook(walletId, createWalletWebhookRequest, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['WebhooksApi.createWalletWebhook']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
          * Create a new webhook
          * @summary Create a new webhook
          * @param {CreateWebhookRequest} [createWebhookRequest] 
@@ -9509,6 +9580,17 @@ export const WebhooksApiFactory = function (configuration?: Configuration, baseP
     const localVarFp = WebhooksApiFp(configuration)
     return {
         /**
+         * Create a new webhook scoped to a wallet
+         * @summary Create a new webhook scoped to a wallet
+         * @param {string} walletId The ID of the wallet to create the webhook for.
+         * @param {CreateWalletWebhookRequest} [createWalletWebhookRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createWalletWebhook(walletId: string, createWalletWebhookRequest?: CreateWalletWebhookRequest, options?: RawAxiosRequestConfig): AxiosPromise<Webhook> {
+            return localVarFp.createWalletWebhook(walletId, createWalletWebhookRequest, options).then((request) => request(axios, basePath));
+        },
+        /**
          * Create a new webhook
          * @summary Create a new webhook
          * @param {CreateWebhookRequest} [createWebhookRequest] 
@@ -9560,6 +9642,17 @@ export const WebhooksApiFactory = function (configuration?: Configuration, baseP
  */
 export interface WebhooksApiInterface {
     /**
+     * Create a new webhook scoped to a wallet
+     * @summary Create a new webhook scoped to a wallet
+     * @param {string} walletId The ID of the wallet to create the webhook for.
+     * @param {CreateWalletWebhookRequest} [createWalletWebhookRequest] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof WebhooksApiInterface
+     */
+    createWalletWebhook(walletId: string, createWalletWebhookRequest?: CreateWalletWebhookRequest, options?: RawAxiosRequestConfig): AxiosPromise<Webhook>;
+
+    /**
      * Create a new webhook
      * @summary Create a new webhook
      * @param {CreateWebhookRequest} [createWebhookRequest] 
@@ -9610,6 +9703,19 @@ export interface WebhooksApiInterface {
  * @extends {BaseAPI}
  */
 export class WebhooksApi extends BaseAPI implements WebhooksApiInterface {
+    /**
+     * Create a new webhook scoped to a wallet
+     * @summary Create a new webhook scoped to a wallet
+     * @param {string} walletId The ID of the wallet to create the webhook for.
+     * @param {CreateWalletWebhookRequest} [createWalletWebhookRequest] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof WebhooksApi
+     */
+    public createWalletWebhook(walletId: string, createWalletWebhookRequest?: CreateWalletWebhookRequest, options?: RawAxiosRequestConfig) {
+        return WebhooksApiFp(this.configuration).createWalletWebhook(walletId, createWalletWebhookRequest, options).then((request) => request(this.axios, this.basePath));
+    }
+
     /**
      * Create a new webhook
      * @summary Create a new webhook
