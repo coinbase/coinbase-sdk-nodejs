52af98c6c251136ae4b4754c35c6bddb37bcad3e
Howard Xie <howard.xie@coinbase.com>
2024-10-09 15:35:58 -0700

    address comments

--- src/tests/webhook_test.ts
+++ src/tests/webhook_test.ts
@@ -214,6 +214,19 @@ describe("Webhook", () => {
 
       expect(webhook.getNotificationURI()).toBe("https://new-url.com/callback");
     });
+    it("should update the webhook address list only", async () => {
+      const webhook = Webhook.init(mockModel);
+      await webhook.update({ eventTypeFilter: { addresses: ["0x1..", "0x2.."] } });
+
+      expect(Coinbase.apiClients.webhook!.updateWebhook).toHaveBeenCalledWith("test-id", {
+        notification_uri: "https://example.com/callback",
+        event_filters: [{ contract_address: "0x...", from_address: "0x...", to_address: "0x..." }],
+        event_type_filter: { addresses: ["0x1..", "0x2.."] },
+      });
+
+      expect(webhook.getNotificationURI()).toBe("https://example.com/callback");
+      expect(webhook.getEventTypeFilter()?.addresses).toEqual(["0x1..", "0x2.."]);
+    });
     it("should update both the webhook notification URI and the list of addresses monitoring", async () => {
       const mockModel: WebhookModel = {
         id: "test-id",
