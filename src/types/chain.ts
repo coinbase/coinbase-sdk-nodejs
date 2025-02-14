a6350a3b966102909c34c793c55696c8fe97fca6
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2025-02-14 12:00:10 -0500

    Changes

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -1,19 +1,11 @@
 import { NetworkIdentifier } from "../client/api";
 
 /**
- * Maps chain IDs to their corresponding Coinbase network IDs
+ * Maps chain IDs to their corresponding Coinbase network IDs. Only SmartWallet related chains are listed here right now.
  */
 export const CHAIN_ID_TO_NETWORK_ID = {
-  1: "ethereum-mainnet",
-  11155111: "ethereum-sepolia",
-  137: "polygon-mainnet",
-  80001: "polygon-mumbai",
   8453: "base-mainnet",
   84532: "base-sepolia",
-  42161: "arbitrum-mainnet",
-  421614: "arbitrum-sepolia",
-  10: "optimism-mainnet",
-  11155420: "optimism-sepolia",
 } as const;
 
 /**
